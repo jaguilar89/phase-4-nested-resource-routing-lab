206803c649e24a864c88c1c4dc7ece73b83c3ab8
Jose Aguilar <jaguilarny@gmail.com>
2022-12-17 15:53:35 -0500

    commit tree e5edb58b852aeab38fdf05385c2779195388781c

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,35 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :resource_not_found_response
 
   def index
     items = Item.all
-    render json: items, include: :user
+
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      render json: items, include: :user
+    else
+      render json: items, include: :user
+    end
   end
 
+  def show
+    item = Item.find(params[:id])
+    render json: item, status: :ok
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    new_item = user.items.create(
+      name: params[:name],
+      description: params[:description],
+      price: params[:price],
+    )
+    render json: new_item, status: :created
+  end
+
+  private
+
+  def resource_not_found_response
+    render json: { error: "Not Found" }, status: :not_found
+  end
 end
